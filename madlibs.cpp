919bcc478d097fb2bd6d63c8a576d0bf30823059
Bnorr <btnorris@mail.csuchico.edu>
2019-09-11 16:31:12 -0700

    MadLib creation

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,32 @@
-//Authors: 
+//Authors:Brian Norris
 #include <iostream>
 #include <string>
 
+
 int main()
 {
+      
+        std::string dogs;
+        std::string fat;
+        std::string walked;
+        int number = 0;
+        std::string shark;
+
+
+        std::cout<<"Enter a plural noun: ";
+        std::cin>>dogs;
+        std::cout<<"Enter an adjective: ";
+        std::cin>>fat;
+        std::cout<<"Enter a verb: ";
+        std::cin>>walked;
+        std::cout<<"Enter a number: ";
+        std::cin>>number;
+        std::cout<<"Enter an animal: ";
+        std::cin>>shark;
+
+        std::cout<<"There were two "<<dogs<<", one was more "<<fat<<" than the other.";
+        std::cout<<" They "<<walked<<" down the "<<number<<" mile hike and got eaten by a "<<shark<<".";
+
 
   return 0;
 }
